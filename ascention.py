32b61b6fc5084aa2e22a3e69eec6e6f23b0d97ca
Valche (fannysil) MacBook Pro <fannysil@student.21-school.ru>
2024-01-01 22:45:59 +0300

    db + code changes

--- ascention.py
+++ ascention.py
@@ -2,53 +2,72 @@ import os
 import requests
 from dotenv import load_dotenv
 import discord
-import json
 from discord.ext import commands
+import sqlite3
 
+#getting tokens
 load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
 stable_diffusion_token = os.getenv('STABLEDIFFUSION_TOKEN')
 
+#results storage path
 sandwich_result = '/tmp/sandwich.png'
+
+#sqlite db shit
+dbase = sqlite3.connect('singlbot.db')
+print('Database opened')
+
+dbase.execute(''' CREATE TABLE IF NOT EXISTS user_info(
+    ID INT PRIMARY KEY NOT NULL,
+    USERNAME TEXT NOT NULL,
+    USER_ID TEXT NOT NULL,
+    COMMAND NOT NULL,
+    CHANNEL NOT NULL) ''')
+
+print('Table created')
+
+def insert_user_info(ID,USERNAME,USER_ID,COMMAND):
+    db.execute(''' INSERT INTO user_info(ID,USERNAME,USER_ID,COMMAND,CHANNEL
+            VALUES(?,?,?,?,?)''',(ID,USERNAME,USER_ID,COMMAND,CHANNEL))
+
+    dbase.commit()
+    print('New user info inserted')
 
 intents = discord.Intents.all()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-def assign_queue():
-    queue_indicator = 0
-    user_queue = queue_indicator += 1
-    return user_queue
 
-def get_username():
-    username = {message.author}
-    return username
-#user_info = {
- #   "queue_position": user_queue,
-  #  "username": {user}
+#@bot.command()
+#async def sandwich
 
-bot_queue = [{
-    "queue_position": user_queue,
-    "username": user_name,
-    "command": user_command
-}]
+ID = 0
 
 @client.event
-async def commands_processing(message):
-    if msg.startswith('>sandwich'):
-        with open("bot_queue.json", "w") as write_file:
-            json.dump(data, write_file)
-    elif msg.startswith('>imgen'):
-        with  open("bot_queue.json", "w") as write_file:
-            json.dump(data, write_file)
-    elif msg.startswith('>hello'):
-        with  open("bot_queue.json", "w") as write_file:
-            json.dump(data, write_file)
-    elif msg.startswith('>info'):
-        with  open("bot_queue.json", "w") as write_file:
-            json.dump(data, write_file)
-    elif msg.startswith('>sketch'):
-        with  open("bot_queue.json", "w") as write_file:
-            json.dump(data, write_file)
-client.run(discord_token)
+async def on_mesage(ctx, message):
+    if message.content.startswith('>sandwich'):
+        ID += 1
+        USERNAME = message.author
+        CHANNEL = message.channel
+        USER_ID =  message.author.id
+        insert_user_info(ID,USERNAME,USER_ID,'>sandwich',CHANNEL)
+#    elif message.content.startswith('>imgen'):
+#        client_id += 1
+#        username = message.author
+#        
+#    elif message.content.startswith('>hello'):
+#        client_id += 1
+#        username = message.author
+#        
+#    elif message.content.startswith('>help'):
+#        client_id += 1
+#        username = message.author
+#        
+#    elif message.content.startswith('>sketch'):
+#        client_id += 1
+#        username = message.author
+#        
+    else:
+        return
 
+client.run(discord_token)
