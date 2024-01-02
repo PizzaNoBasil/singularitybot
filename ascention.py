d0e0b6e5fa6520418c0700816be61cf2c3db2795
Valche (fannysil) MacBook Pro <fannysil@student.21-school.ru>
2024-01-02 04:07:10 +0300

    db logging acquired

--- ascention.py
+++ ascention.py
@@ -5,8 +5,6 @@ import discord
 from discord.ext import commands
 import sqlite3
 
-global ID
-
 #getting tokens
 load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
@@ -17,10 +15,11 @@ sandwich_result = '/tmp/sandwich.png'
 
 #sqlite db shit
 dbase = sqlite3.connect('singlbot.db')
+dbase.execute('pragma journal_mode=wal')
 print('Database opened')
 
 dbase.execute(''' CREATE TABLE IF NOT EXISTS user_info(
-    ID INT PRIMARY KEY NOT NULL,
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
     USERNAME TEXT NOT NULL,
     USER_ID TEXT NOT NULL,
     COMMAND TEXT NOT NULL,
@@ -28,13 +27,13 @@ dbase.execute(''' CREATE TABLE IF NOT EXISTS user_info(
 
 print('Table created')
 
-def insert_user_info(ID,USERNAME,USER_ID,COMMAND,CHANNEL):
-    dbase.execute('''INSERT INTO user_info (ID,USERNAME,USER_ID,COMMAND,CHANNEL)
-            VALUES (?,?,?,?,?)''',(ID,USERNAME,USER_ID,COMMAND,CHANNEL))
+def insert_user_info(USERNAME,USER_ID,COMMAND,CHANNEL):
+    dbase.execute('''INSERT INTO user_info (USERNAME,USER_ID,COMMAND,CHANNEL)
+            VALUES (?,?,?,?)''',(USERNAME,USER_ID,COMMAND,CHANNEL))
 
     dbase.commit()
     print('New user info inserted')
-
+    
 #bot setup
 intents = discord.Intents.all()
 intents.message_content = True
@@ -44,32 +43,22 @@ client = discord.Client(intents=intents)
 #bot commands
 @client.event
 async def on_message(message):
-    global ID
-    ID = 0
+
+    USERNAME = str(message.author)
+    CHANNEL = message.channel.id
+    USER_ID =  message.author.id
+
     if message.content.startswith('>sandwich'):
-        ID += 1
-        USERNAME = message.author
-        CHANNEL = message.channel.id
-        USER_ID =  message.author.id
-        print("ID = ", ID, "USERNAME = ", USERNAME, "CHANNEL = ", CHANNEL, "USER_ID = ", USER_ID)
-        insert_user_info(ID,USERNAME,USER_ID,'>sandwich',CHANNEL)
-#    elif message.content.startswith('>imgen'):
-#        client_id += 1
-#        username = message.author
-#        
-#    elif message.content.startswith('>hello'):
-#        client_id += 1
-#        username = message.author
-#        
+        insert_user_info(USERNAME,USER_ID,'>sandwich',CHANNEL)
+
+    elif message.content.startswith('>imgen'):
+        insert_user_info(USERNAME,USER_ID,'>imgen',CHANNEL)
+
+    elif message.content.startswith('>hello'):
+        await message.channel.send('Привет, я - бот, разработанный Singularity с целью научиться работать с api discord.py для продвижения его скиллов в Python!')
 #    elif message.content.startswith('>help'):
-#        client_id += 1
-#        username = message.author
-#        
-#    elif message.content.startswith('>sketch'):
-#        client_id += 1
-#        username = message.author
-#        
-#    else:
-#        return
+
+    elif message.content.startswith('>sketch'):
+        insert_user_info(USERNAME,USER_ID,'>sketch',CHANNEL)
 
 client.run(discord_token)
